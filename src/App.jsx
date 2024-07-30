254e99cdf9d54f259f7f195608943e68d327f674
Himanshu Rawat <himanshurawat90k@gmail.com>
2024-07-30 22:02:04 +0530

    Added Navbar, Hero, and JobListings sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
+import JobListings from './components/JobListings';
+
 const App = () => {
-  return <div className="text-5xl">  App </div>;
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    <HomeCards />
+    <JobListings />
+    <section class="m-auto max-w-lg my-10 px-6">
+      <a
+        href="jobs.html"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
+    </>
+  )
 };
 
 export default App;
