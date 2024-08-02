2ec349be4bae51a6061fe49870e1d393cad9d94b
Himanshu Rawat <himanshurawat90k@gmail.com>
2024-08-02 21:41:32 +0530

    Attached Json file as mock server data

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,3 +1,5 @@
+import jobs from '../jobs.json';
+
 const JobListings = () => {
     return ( 
         <section class="bg-blue-50 px-4 py-10">
@@ -7,28 +9,30 @@ const JobListings = () => {
           </h2>
           <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
             {/* <!-- Job Listing 1 --> */}
-            <div class="bg-white rounded-xl shadow-md relative">
+            {jobs.map( (job) => {
+              return (
+              <div class="bg-white rounded-xl shadow-md relative">
               <div class="p-4">
                 <div class="mb-6">
-                  <div class="text-gray-600 my-2">Full-Time</div>
-                  <h3 class="text-xl font-bold">Senior React Developer</h3>
+                  <div class="text-gray-600 my-2">{job.type}</div>
+                  <h3 class="text-xl font-bold">{job.title}</h3>
                 </div>
 
                 <div class="mb-5">
-                 We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript...
+                 {job.description}
                 </div>
 
-                <h3 class="text-indigo-500 mb-2">$70 - $80K / Year</h3>
+                <h3 class="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
                 <div class="border border-gray-100 mb-5"></div>
 
                 <div class="flex flex-col lg:flex-row justify-between mb-4">
                   <div class="text-orange-700 mb-3">
                     <i class="fa-solid fa-location-dot text-lg"></i>
-                    Boston, MA
+                    {job.location}
                   </div>
                   <a
-                    href="job.html"
+                    href={`/jobs/${job.id}`}
                     class="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                   >
                    Read More
@@ -36,66 +40,8 @@ const JobListings = () => {
                 </div>
               </div>
             </div>
-            {/* <!-- Job Listing 2 --> */}
-            <div class="bg-white rounded-xl shadow-md relative">
-              <div class="p-4">
-                <div class="mb-6">
-                  <div class="text-gray-600 my-2">Remote</div>
-                  <h3 class="text-xl font-bold">Front-End Engineer (React)</h3>
-                </div>
-
-                <div class="mb-5">
-                 Join our team as a Front-End Developer in sunny Miami, FL. We are looking for a motivated individual with a passion...
-                </div>
-
-                <h3 class="text-indigo-500 mb-2">$70K - $80K / Year</h3>
-
-                <div class="border border-gray-100 mb-5"></div>
-
-                <div class="flex flex-col lg:flex-row justify-between mb-4">
-                  <div class="text-orange-700 mb-3">
-                    <i class="fa-solid fa-location-dot text-lg"></i>
-                    Miami, FL
-                  </div>
-                  <a
-                    href="job.html"
-                    class="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-                  >
-                   Read More
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* <!-- Job Listing 3 --> */}
-            <div class="bg-white rounded-xl shadow-md relative">
-              <div class="p-4">
-                <div class="mb-6">
-                  <div class="text-gray-600 my-2">Remote</div>
-                  <h3 class="text-xl font-bold">React.js Developer</h3>
-                </div>
-
-                <div class="mb-5">
-                  Are you passionate about front-end development? Join our team in vibrant Brooklyn, NY, and work on exciting projects that make a difference...
-                </div>
-
-                <h3 class="text-indigo-500 mb-2">$70K - $80K / Year</h3>
-
-                <div class="border border-gray-100 mb-5"></div>
-
-                <div class="flex flex-col lg:flex-row justify-between mb-4">
-                  <div class="text-orange-700 mb-3">
-                    <i class="fa-solid fa-location-dot text-lg"></i>
-                    Brooklyn, NY
-                  </div>
-                  <a
-                    href="job.html"
-                    class="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-                  >
-                   Read More
-                  </a>
-                </div>
-              </div>
-            </div>
+            )
+            })}
           </div>
         </div>
         </section>
