d4fb61b9f3037a1dd8a6272bf3d2aaae9dca7da9
Himanshu Rawat <himanshurawat90k@gmail.com>
2024-08-20 17:13:56 +0530

    added jobs page

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,19 +1,19 @@
 import jobs from '../jobs.json';
 import JobListing from './JobListing';
 
-const JobListings = () => {
+const JobListings = ({isHome = false}) => {
 
-  const recentJobs = jobs.slice(0, 3);
+  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
 
     return ( 
         <section class="bg-blue-50 px-4 py-10">
         <div class="container-xl lg:container m-auto">
           <h2 class="text-3xl font-bold text-indigo-500 mb-6 text-center">
-            Browse Jobs
+            {isHome ? 'Recent Jobs' : 'Browse Jobs'}
           </h2>
           <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
             {/* <!-- Job Listing 1 --> */}
-            {recentJobs.map( (job) => {
+            {jobListings.map( (job) => {
               return (
                 <JobListing key={job.id} job={job}/>
             )
