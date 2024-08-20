d8b531fe86e306608add20a28590e2d184b01785
Himanshu Rawat <himanshurawat90k@gmail.com>
2024-08-20 22:37:56 +0530

    Removed local file dependency for data.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,24 +1,48 @@
-import jobs from '../jobs.json';
+import { useState, useEffect } from 'react';
 import JobListing from './JobListing';
+import Spinner from './Spinner';
 
 const JobListings = ({isHome = false}) => {
+    const [ jobs, setJobs ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
 
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+    useEffect(() => {
+      const fetchJobs = async () => {
+        const apiUrl = isHome 
+        ? 'http://localhost:8000/jobs?_limit=3'
+        : 'http://localhost:8000/jobs'
 
+        try {
+          const res = await fetch(apiUrl);
+          const data = await res.json();
+          setJobs(data);
+        } catch (error) {
+          console.log('Error fetching data', error);
+        } finally {
+          setLoading(false);
+        }
+      }
+
+      fetchJobs ();
+    }, []);
+  
     return ( 
         <section class="bg-blue-50 px-4 py-10">
         <div class="container-xl lg:container m-auto">
           <h2 class="text-3xl font-bold text-indigo-500 mb-6 text-center">
             {isHome ? 'Recent Jobs' : 'Browse Jobs'}
           </h2>
-          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {/* <!-- Job Listing 1 --> */}
-            {jobListings.map( (job) => {
-              return (
-                <JobListing key={job.id} job={job}/>
-            )
-            })}
-          </div>
+            {loading ? (
+              <Spinner loading={ loading } />
+            ) : (
+              <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {jobs.map( (job) => {
+                  return (
+                    <JobListing key={job.id} job={job}/>
+                  )
+                })}
+              </div>
+            )}
         </div>
         </section>
     )
